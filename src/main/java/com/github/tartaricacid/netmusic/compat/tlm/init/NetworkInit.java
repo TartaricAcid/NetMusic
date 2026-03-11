87cd73bc7dd983bfc07843c83c235a475c0bd3f9
IMG <1973581872@qq.com>
2026-03-12 00:46:55 +0800

    修正女仆音响背包停止音乐时可能在服务端加载客户端类的问题

--- src/main/java/com/github/tartaricacid/netmusic/compat/tlm/init/NetworkInit.java
+++ src/main/java/com/github/tartaricacid/netmusic/compat/tlm/init/NetworkInit.java
@@ -4,6 +4,7 @@ import com.github.tartaricacid.netmusic.client.init.ClientReceiverRegistry;
 import com.github.tartaricacid.netmusic.compat.tlm.message.MaidMusicToClientMessage;
 import com.github.tartaricacid.netmusic.compat.tlm.message.MaidStopMusicMessage;
 import com.github.tartaricacid.netmusic.compat.tlm.receiver.MaidMusicToClientMessageReceiver;
+import com.github.tartaricacid.netmusic.compat.tlm.receiver.MaidStopMusicMessageReceiver;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
@@ -11,7 +12,7 @@ public class NetworkInit {
     @Environment(EnvType.CLIENT)
     public static void clientInit() {
         ClientReceiverRegistry.registerReceiver(MaidMusicToClientMessage.ID, MaidMusicToClientMessageReceiver::handle);
-        ClientReceiverRegistry.registerReceiver(MaidStopMusicMessage.ID, MaidStopMusicMessage::handle);
+        ClientReceiverRegistry.registerReceiver(MaidStopMusicMessage.ID, MaidStopMusicMessageReceiver::handle);
     }
 
     public static void serverInit() {
