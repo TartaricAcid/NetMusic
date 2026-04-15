9fa894f594543de4436719b4df9a630c9dcee7e4
tartaric_acid <baka943@qq.com>
2026-04-15 18:54:47 +0800

    优化搜索功能

--- src/main/java/com/github/tartaricacid/netmusic/api/search/NeteaseMusicSearch.java
+++ src/main/java/com/github/tartaricacid/netmusic/api/search/NeteaseMusicSearch.java
@@ -5,29 +5,35 @@ import com.github.tartaricacid.netmusic.api.NetWorker;
 import com.github.tartaricacid.netmusic.api.WebApi;
 import com.google.common.net.HttpHeaders;
 
-import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.ProxySelector;
+import java.net.SocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 public class NeteaseMusicSearch {
+    private static final HttpClient SEARCH_CLIENT = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(30))
+            .proxy(new ProxySelector() {
+                @Override
+                public List<Proxy> select(URI uri) {
+                    Proxy proxy = NetWorker.getProxyFromConfig();
+                    return List.of(Objects.requireNonNullElse(proxy, Proxy.NO_PROXY));
+                }
+
+                @Override
+                public void connectFailed(URI uri, SocketAddress sa, java.io.IOException ioe) {
+                }
+            }).build();
+
     public static CompletableFuture<HttpResponse<String>> searchSongs(String keywords) {
-        //Proxy proxy = NetWorker.getProxyFromConfig();
-        //InetSocketAddress addr = (InetSocketAddress) proxy.address();
-        //ProxySelector selector = ProxySelector.of(addr);
-
         URI uri = URI.create(WebApi.getSearchUrl(keywords, WebApi.TYPE_SONG, 5));
-
-        HttpClient client = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(30))
-                //.proxy(selector)
-                .build();
-
         HttpRequest request = HttpRequest.newBuilder()
                 .timeout(Duration.ofSeconds(30))
                 .header(HttpHeaders.REFERER, NetEaseMusic.getReferer())
@@ -35,6 +41,6 @@ public class NeteaseMusicSearch {
                 .header(HttpHeaders.USER_AGENT, NetEaseMusic.getUserAgent())
                 .uri(uri).GET().build();
 
-        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        return SEARCH_CLIENT.sendAsync(request, HttpResponse.BodyHandlers.ofString());
     }
 }
