fc68aff1a8c4b395b0a34454f1f6555432be77e3
tartaric_acid <baka943@qq.com>
2026-05-05 20:25:43 +0800

    修改注册方式 - 修改精妙背包的问题

--- src/main/java/com/github/tartaricacid/netmusic/client/api/AudioStreamHandlerManager.java
+++ src/main/java/com/github/tartaricacid/netmusic/client/api/AudioStreamHandlerManager.java
@@ -1,9 +1,10 @@
 package com.github.tartaricacid.netmusic.client.api;
 
+import com.github.tartaricacid.netmusic.NetMusic;
 import com.github.tartaricacid.netmusic.client.api.implement.*;
+import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import net.neoforged.neoforge.common.NeoForge;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -15,25 +16,25 @@ public final class AudioStreamHandlerManager {
     private static List<IAudioStreamHandler> HANDLERS = Lists.newArrayList();
 
     public static void init() {
-        AudioStreamHandlerManager manager = new AudioStreamHandlerManager();
-        HANDLERS = Lists.newArrayList();
-
-        AudioStreamHandlerEvent event = new AudioStreamHandlerEvent(manager);
-        NeoForge.EVENT_BUS.post(event);
-
         // 注册自己的 handler
-        manager.registerHandler(new CnrM3u8Handler());
-        manager.registerHandler(new M3u8Handler());
-        manager.registerHandler(new NetEaseHttpHandler());
-        manager.registerHandler(new LocalFileHandler());
-        manager.registerHandler(new DirectHttpHandler());
+        registerHandler(new CnrM3u8Handler());
+        registerHandler(new M3u8Handler());
+        registerHandler(new NetEaseHttpHandler());
+        registerHandler(new LocalFileHandler());
+        registerHandler(new DirectHttpHandler());
 
         // 按优先级排序
         HANDLERS.sort((h1, h2) -> Integer.compare(h2.getPriority(), h1.getPriority()));
         HANDLERS = ImmutableList.copyOf(HANDLERS);
     }
 
-    void registerHandler(IAudioStreamHandler handler) {
+    public static void registerHandler(IAudioStreamHandler handler) {
+        if (HANDLERS instanceof ImmutableCollection<?>) {
+            NetMusic.LOGGER.error("Failed to register audio stream handler {}, " +
+                                  "you should register it before the load complete event",
+                    handler.getClass().getName());
+            return;
+        }
         HANDLERS.add(handler);
     }
 
--- src/main/java/com/github/tartaricacid/netmusic/compat/sbackpack/NetMusicDiscHandler.java
+++ src/main/java/com/github/tartaricacid/netmusic/compat/sbackpack/NetMusicDiscHandler.java
@@ -28,13 +28,14 @@ public class NetMusicDiscHandler implements IDiscHandler<ItemMusicCD.SongInfo> {
     @Override
     public void playDisc(ServerLevel serverLevel, BlockPos position, UUID storageUuid, ItemStack discItemStack, Runnable onFinished) {
         getSongInfo(discItemStack, serverLevel).ifPresent(songInfo -> {
+            Vec3 pos = Vec3.atCenterOf(position);
+            long finishTime = serverLevel.getGameTime() + getMusicLengthInTicks(songInfo);
+            ServerStorageSoundHandler.putSoundInfo(serverLevel, storageUuid, onFinished, pos, finishTime);
+
             ItemMusicCD.SongInfo copy = songInfo.clone();
             MusicPlayResolverManager.resolve(copy).thenAcceptAsync(resolved -> {
-                Vec3 pos = Vec3.atCenterOf(position);
                 NetMusicDiscPayload payload = new NetMusicDiscPayload(storageUuid, songInfo, resolved.songUrl, position);
                 PacketDistributor.sendToPlayersNear(serverLevel, null, pos.x, pos.y, pos.z, 128, payload);
-                long finishTime = serverLevel.getGameTime() + getMusicLengthInTicks(resolved);
-                ServerStorageSoundHandler.putSoundInfo(serverLevel, storageUuid, onFinished, pos, finishTime);
             }, serverLevel.getServer());
         });
     }
@@ -42,12 +43,13 @@ public class NetMusicDiscHandler implements IDiscHandler<ItemMusicCD.SongInfo> {
     @Override
     public void playDisc(ServerLevel serverLevel, Vec3 position, UUID storageUuid, ItemStack discItemStack, int entityId, Runnable onFinished) {
         getSongInfo(discItemStack, serverLevel).ifPresent(songInfo -> {
+            long finishTime = serverLevel.getGameTime() + getMusicLengthInTicks(songInfo);
+            ServerStorageSoundHandler.putSoundInfo(serverLevel, storageUuid, onFinished, position, finishTime);
+
             ItemMusicCD.SongInfo copy = songInfo.clone();
             MusicPlayResolverManager.resolve(copy).thenAcceptAsync(resolved -> {
                 NetMusicDiscPayload payload = new NetMusicDiscPayload(storageUuid, songInfo, resolved.songUrl, entityId);
                 PacketDistributor.sendToPlayersNear(serverLevel, null, position.x, position.y, position.z, 128, payload);
-                long finishTime = serverLevel.getGameTime() + getMusicLengthInTicks(songInfo);
-                ServerStorageSoundHandler.putSoundInfo(serverLevel, storageUuid, onFinished, position, finishTime);
             }, serverLevel.getServer());
         });
     }
