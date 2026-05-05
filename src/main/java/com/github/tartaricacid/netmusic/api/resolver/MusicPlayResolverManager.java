2f0b92f4d9eab395e271dad507c846f8c2ebf2f5
tartaric_acid <baka943@qq.com>
2026-05-05 19:36:45 +0800

    修改注册方式

--- src/main/java/com/github/tartaricacid/netmusic/api/resolver/MusicPlayResolverManager.java
+++ src/main/java/com/github/tartaricacid/netmusic/api/resolver/MusicPlayResolverManager.java
@@ -2,9 +2,9 @@ package com.github.tartaricacid.netmusic.api.resolver;
 
 import com.github.tartaricacid.netmusic.NetMusic;
 import com.github.tartaricacid.netmusic.item.ItemMusicCD;
+import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import net.minecraftforge.common.MinecraftForge;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -13,19 +13,19 @@ public final class MusicPlayResolverManager {
     private static List<IAsyncSongUrlResolver> RESOLVERS = Lists.newArrayList();
 
     public static void init() {
-        MusicPlayResolverManager manager = new MusicPlayResolverManager();
-        RESOLVERS = Lists.newArrayList();
-
-        SongUrlResolverEvent event = new SongUrlResolverEvent(manager);
-        MinecraftForge.EVENT_BUS.post(event);
-
         // RESOLVERS.add(new DefaultVipResolver());
 
         RESOLVERS.sort((h1, h2) -> Integer.compare(h2.getPriority(), h1.getPriority()));
         RESOLVERS = ImmutableList.copyOf(RESOLVERS);
     }
 
-    void registerResolver(IAsyncSongUrlResolver resolver) {
+    public static void registerResolver(IAsyncSongUrlResolver resolver) {
+        if (RESOLVERS instanceof ImmutableCollection<?>) {
+            NetMusic.LOGGER.error("Failed to register song URL resolver {}, " +
+                                  "you should register it before the load complete event",
+                    resolver.getClass().getName());
+            return;
+        }
         RESOLVERS.add(resolver);
     }
 
