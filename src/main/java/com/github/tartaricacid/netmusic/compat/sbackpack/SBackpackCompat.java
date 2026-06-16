0357c289e3bc42cdf21ea357fc40f1753369564c
IMG <1973581872@qq.com>
2026-06-16 21:02:54 +0800

    修正与精妙背包的兼容问题

--- src/main/java/com/github/tartaricacid/netmusic/compat/sbackpack/SBackpackCompat.java
+++ src/main/java/com/github/tartaricacid/netmusic/compat/sbackpack/SBackpackCompat.java
@@ -2,15 +2,27 @@ package com.github.tartaricacid.netmusic.compat.sbackpack;
 
 import com.github.tartaricacid.netmusic.init.CompatRegistry;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.network.simple.SimpleChannel;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 
 public class SBackpackCompat {
+    @SuppressWarnings("Convert2MethodRef")
     public static void register() {
-        ModList.get().getModContainerById(CompatRegistry.SC).ifPresent(modContainer -> {
-            ArtifactVersion version = modContainer.getModInfo().getVersion();
-            if (CompatRegistry.SC_VERSION_RANGE.containsVersion(version)) {
-                SBackpackCompatInner.register();
-            }
-        });
+        checkSBackpackLoad(() -> SBackpackCompatInner.register());
+    }
+
+    public static void initNetwork(SimpleChannel channel) {
+        checkSBackpackLoad(() -> SBackpackCompatInner.initNetwork(channel));
+    }
+
+    public static void checkSBackpackLoad(Runnable runnable) {
+        if (ModList.get().isLoaded(CompatRegistry.SC)) {
+            ModList.get().getModContainerById(CompatRegistry.SC).ifPresent(modContainer -> {
+                ArtifactVersion version = modContainer.getModInfo().getVersion();
+                if (CompatRegistry.SC_VERSION_RANGE.containsVersion(version)) {
+                    runnable.run();
+                }
+            });
+        }
     }
 }
